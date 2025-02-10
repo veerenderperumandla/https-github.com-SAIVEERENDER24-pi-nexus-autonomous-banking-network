ce69a2e0c9bea0dae15d68126469d31204381663
KOSASIH <kosasihg88@gmail.com>
2025-02-11 00:01:45 +0700

    merge

--- src/api/routes/adminRoutes.js
+++ src/api/routes/adminRoutes.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const { getAllUsers, getAllTransactions } = require('../services/adminService');
 const authMiddleware = require('../middleware/authMiddleware');
+<<<<<<< HEAD
 const authorizationMiddleware = require('../middleware/authorizationMiddleware');
+=======
+>>>>>>> 8ae95fbf (Create adminRoutes.js)
 
 const router = express.Router();
 
 // Route to get all users
+<<<<<<< HEAD
 router.get('/users', authMiddleware, authorizationMiddleware(['admin']), getAllUsers);
 
 // Route to get all transactions
 router.get('/transactions', authMiddleware, authorizationMiddleware(['admin']), getAllTransactions);
+=======
+router.get('/users', authMiddleware, getAllUsers);
+
+// Route to get all transactions
+router.get('/transactions', authMiddleware, getAllTransactions);
+>>>>>>> 8ae95fbf (Create adminRoutes.js)
 
 module.exports = router;
